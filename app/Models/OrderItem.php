c59712eb11c575492cf9f766f5448282e3d2b9aa
Mohd Saleem <49087747+unosaleem@users.noreply.github.com>
2024-10-09 20:01:23 +0530

    order item Database

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'product_name',
+        'product_image',
+        'qty',
+        'coupon_code',
+        'price',
+        'tax_rate',
+        'tax_amount',
+        'total_price',
+        'payment_method',
+        'status',
+    ];
 }
--- database/migrations/2024_10_09_141750_create_order_items_table.php
+++ database/migrations/2024_10_09_141750_create_order_items_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->string('order_id');  // Unique order number
+            $table->string('product_id');  // Unique order number
+            $table->string('product_name');  // Unique order number
+            $table->string('product_image');  // Unique order number
+            $table->integer('qty');  // Order Item QTY
+            $table->decimal('price', 10, 2);  //  price of the Product
+            $table->string('tax_rate');  // Tax Rate of Product
+            $table->decimal('tax_amount', 10, 2);  // TAx price of the Product
+            $table->decimal('total_price', 10, 2);  // Total price of the Product
+            $table->enum('status', ['pending','new','processing','completed','cancelled','refunded'])->default('pending');  // Status of the order
             $table->timestamps();
         });
     }
